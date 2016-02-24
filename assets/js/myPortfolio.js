5058f45bfb2efd9229c260e1698f7a27fbc3df32
Sam <sam.ringleman@gmail.com>
2016-02-24 09:13:04 -0700

    added portfolio template

---
+++ assets/js/myPortfolio.js
@@ -0,0 +1,15 @@
+var one = "#614EFF",
+    two = "#FFE386",
+    three = "#000";
+
+$(window).on("scroll", function(){
+  if ($(document).scrollTop() >= $("#one").position().top){
+    $('body').css("background", one);
+  };
+    if ($(document).scrollTop() > $("#two").position().top){
+    $('body').css("background", two);
+  };
+    if ($(document).scrollTop() > $("#three").position().top){
+    $('body').css("background", three);
+  };
+})
