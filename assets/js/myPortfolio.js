b961efd0194d15348c11349d981cd478be8ae9fb
Sam <sam.ringleman@gmail.com>
2016-02-24 09:18:35 -0700

    minor change to js

--- assets/js/myPortfolio.js
+++ assets/js/myPortfolio.js
@@ -5,11 +5,14 @@ var one = "#614EFF",
 $(window).on("scroll", function(){
   if ($(document).scrollTop() >= $("#one").position().top){
     $('body').css("background", one);
+    $("body").css("color", "#FFF");
   };
     if ($(document).scrollTop() > $("#two").position().top){
     $('body').css("background", two);
+    $("body").css("color", "#000");
   };
     if ($(document).scrollTop() > $("#three").position().top){
     $('body').css("background", three);
+    $("body").css("color", "#FFF");
   };
 })
