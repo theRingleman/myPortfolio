834a6f3dbe82cb8a6b33a8e67f7a731be6f09e22
Sam <sam.ringleman@gmail.com>
2016-02-25 08:20:05 -0700

    Added more content and links out to social networks as well as completed task for making page jump to certain areas, also I changed the font.

--- assets/js/myPortfolio.js
+++ assets/js/myPortfolio.js
@@ -1,18 +1,23 @@
-var one = "#614EFF",
+var one = "#3D5B99",
     two = "#FFE386",
-    three = "#000";
+    three = "#000",
+    four = "#AB7A64";
 
 $(window).on("scroll", function(){
   if ($(document).scrollTop() >= $("#one").position().top){
-    $('body').css("background", one);
-    $("body").css("color", "#000");
+      $('body').css("background", one);
+      $("body").css("color", "#000");
   };
     if ($(document).scrollTop() > $("#two").position().top){
-    $('body').css("background", two);
-    $("body").css("color", "#000");
+      $('body').css("background", two);
+      $("body").css("color", "#000");
   };
     if ($(document).scrollTop() > $("#three").position().top){
-    $('body').css("background", three);
-    $("body").css("color", "#FFF");
+      $('body').css("background", three);
+      $("body").css("color", "#FFF");
+  };
+    if ($(document).scrollTop() > $("#four").position().top){
+      $('body').css("background", four);
+      $("body").css("color", "#FFF");
   };
 })
